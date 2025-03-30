0eb52ae58efd8f58ebcd3e0bb72cb5620692bb27
Kyeong-Ha <rudgk030@hanyang.ac.kr>
2025-03-30 14:18:25 +0900

    Configure Docker & Docker-Compose build preferences

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,37 +1,13 @@
 #!/bin/sh
 
-python manage.py migrate
-
-if [ "$DJANGO_ENV" = "production" ]; then
-    python manage.py collectstatic --noinput
-    gunicorn cert_management.wsgi:application --bind 0.0.0.0:8000
-else
-    # Start the Django development server
-    python manage.py runserver 0.0.0.0:8000
-fi
-
-
-#!/bin/sh
-
-if [ "$DATABASE" = "postgres" ]
-then
-    echo "Waiting for postgres..."
-
-    while ! nc -z $SQL_HOST $SQL_PORT; do
-      sleep 0.5
-    done
-
-    echo "PostgreSQL started"
-fi
-
-# Uncomment below to flush db e.g. after running tests
-# Just make sure you really mean it 
-# python manage.py flush --no-input
-
-# We have base custom user model so need to makemigrations out of box
-python manage.py makemigrations core
-
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 
-exec "$@"
+if [ "$DB_ENV" = "production" ]; then
+    echo "Production Mode started."
+    gunicorn config.wsgi:application --bind 0.0.0.0:8000
+elif [ "$DB_ENV" = "development" ]; then
+    echo "Development Mode started."
+    python manage.py runserver 0.0.0.0:8000
+fi
