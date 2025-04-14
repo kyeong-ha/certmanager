af28227f8e52036e67e508ce9168e3cb6bd3a6af
Kyeong-Ha <rudgk030@hanyang.ac.kr>
2025-04-14 22:05:41 +0900

    Initial Setup for AWS EC2 Service Integration

--- backend/config/urls.py
+++ backend/config/urls.py
@@ -3,5 +3,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('certificate/', include('apps.certificate.urls')), 
+    path('/', include('apps.certificate.urls')), 
 ]
