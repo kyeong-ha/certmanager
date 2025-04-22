744b078c7d6ca1e255ee5fdecb3c2af26281b542
kyeong-ha <rudgk030@hanyang.ac.kr>
2025-04-22 16:23:15 +0900

    Maintenance: Categorize code by URL(=Model)

--- backend/api/cert/serializers/CertificateSerializer.py
+++ backend/api/cert/serializers/CertificateSerializer.py
@@ -1,35 +1,41 @@
 from rest_framework import serializers
 from api.cert.models.Certificate import Certificate
 from api.edu.models.EducationCenter import EducationCenter
-from api.edu.serializers.EducationCenterSerializer import EducationCenterSerializer
 from api.logs.serializers.ReissueLogSerializer import ReissueLogSerializer
 from api.user.models.User import User
 from api.user.serializers.UserSerializer import UserSerializer
 
 class CertificateSerializer(serializers.ModelSerializer):
     uuid = serializers.ReadOnlyField()
-    user = UserSerializer()
-    education_center = EducationCenterSerializer()
+    user = UserSerializer(read_only=True)
+    education_center = serializers.SerializerMethodField()
     reissue_logs = ReissueLogSerializer(many=True, read_only=True)
 
     class Meta:
         model = Certificate
         fields = '__all__'
         read_only_fields = ['created_at', 'updated_at']
-    
+
+    def get_user(self, obj):
+        from api.user.serializers.UserSerializer import UserSerializer
+        return UserSerializer(obj.user).data
+
+    def get_education_center(self, obj):
+        from api.edu.serializers.EducationCenterSerializer import EducationCenterSerializer
+        return EducationCenterSerializer(obj.education_center).data
+
     def create(self, validated_data):
         user_data = validated_data.pop('user')
-        education_data = validated_data.pop('education_center')
+        edu_data = validated_data.pop('education_center')
 
         user, _ = User.objects.get_or_create(**user_data)
-        education_center, _ = EducationCenter.objects.get_or_create(**education_data)
+        education_center, _ = EducationCenter.objects.get_or_create(**edu_data)
 
-        certificate = Certificate.objects.create(
+        return Certificate.objects.create(
             user=user,
             education_center=education_center,
             **validated_data
         )
-        return certificate
 
     def update(self, instance, validated_data):
         user_data = validated_data.pop('user', None)
