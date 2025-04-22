15a13658a2b0068f08f9a8b78aaacba9a7224d14
kyeong-ha <rudgk030@hanyang.ac.kr>
2025-04-22 16:21:51 +0900

    Maintenance: Renamed PK variables in the model

--- backend/api/logs/models/ReissueLog.py
+++ backend/api/logs/models/ReissueLog.py
@@ -5,7 +5,7 @@ from api.cert.models.Certificate import Certificate
 
 class ReissueLog(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
-    certificate = models.ForeignKey(Certificate, on_delete=models.CASCADE, related_name='reissue_logs')
+    certificate_uuid = models.ForeignKey(Certificate, on_delete=models.CASCADE, related_name='reissue_logs')
     reissue_date = models.DateField(default=timezone.now)
     delivery_type = models.CharField(max_length=10, choices=[('선불', '선불'), ('착불', '착불')], default='선불')
     reissue_cost = models.IntegerField(null=True, blank=True)
