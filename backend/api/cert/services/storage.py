5eadd3138de0c39b535d4c5ead736c2c7afe3888
KyeongHa <rudgk030@hanyang.ac.kr>
2025-05-05 23:07:26 +0900

    Implementation: Added ability to save cert's copy

---
+++ backend/api/cert/services/storage.py
@@ -0,0 +1,8 @@
+import os
+from django.core.files.storage import FileSystemStorage
+
+class OverwriteStorage(FileSystemStorage):
+    def get_available_name(self, name, max_length=None):
+        if self.exists(name):
+            os.remove(os.path.join(self.location, name))
+        return name
--- backend/api/cert/urls.py
+++ backend/api/cert/urls.py
@@ -2,11 +2,13 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 from api.cert.views.CertificateView import CertificateViewSet
 from api.cert.views.searchView import search
+from api.cert.views.IssueView import issue
 
 router = DefaultRouter()
 router.register('', CertificateViewSet, basename='cert')
 
 urlpatterns = [
-    path('search/', search, name='search-certificates'),
+    path('issue/', issue, name='issue'),
+    path('search/', search, name='search'),
     path('', include(router.urls)), 
 ]
