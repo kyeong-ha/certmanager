95078539c024f7f811d41ed35da28b15ea11b230
KyeongHa <rudgk030@hanyang.ac.kr>
2025-05-05 21:18:50 +0900

    Maintenance: Create a ‘copy_file’ in cert model

--- backend/api/cert/models/Certificate.py
+++ backend/api/cert/models/Certificate.py
@@ -1,5 +1,8 @@
 import uuid
 from django.db import models
+from utils.storage import OverwriteStorage
+from api.cert.models.upload_path import certificate_pdf_upload_path
+
 from api.edu.models.EducationCenter import EducationCenter
 from api.user.models.User import User
 
@@ -7,7 +10,8 @@ class Certificate(models.Model):
     uuid = models.UUIDField(primary_key=True, default=uuid.uuid4, editable=False)
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name='certificates')
     education_center = models.ForeignKey(EducationCenter, null=True, on_delete=models.CASCADE, related_name='certificates')
-
+    copy_file = models.FileField(upload_to=certificate_pdf_upload_path, storage=OverwriteStorage(), blank=True, null=True )
+    
     course_name = models.CharField(max_length=100, null=True, blank=True) # 자격과정
     issue_type = models.CharField(max_length=10, null=True, blank=True) # 분류코드 (HS || HN || S || J || N)
     issue_number = models.CharField(max_length=30, unique=True)  # 발급번호
